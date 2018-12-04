8c5b0523ed55761929c739329010a2a07e0749a4
char55 <haras.charlotte@gmail.com>
2018-12-04 01:13:40 +0000

    small bug remains

--- KATA9_squareCode.js
+++ KATA9_squareCode.js
@@ -26,20 +26,49 @@ function removeSpaces(input) {
 }
 
 
+function makeSquare(input) {
+  const squareSize = Math.floor(Math.sqrt(input.length));
+  const square = []
+  for (let i = 0 ; i <= squareSize; i++) {
+    square[i] = [];
+  }
+
+  let i = 0;
+
+  for (let j = 0; j < input.length; j++) {
+    if ( j % squareSize === 0 && j !== 0 ) {  
+      i++;
+    }
+    square[i].push(input[j]);
+  }
+
+  return square
+}
+
+
 function squareCode(input) {
 
   const arrayLettersOnly = removeSpaces(input)
   if (arrayLettersOnly !== null ) {
-    const squareSize = Math.floor(Math.sqrt(inputArrayNoSpaces.length));
+    const squareArray = makeSquare(arrayLettersOnly)
 
-    let count = 1;
-  
-    for (let i = 0; i < inputArrayNoSpaces.length; i++) {
-      count ++
-      if (count > squareSize) {
-        count = 1
-      }
+    const coded = [];
+    for (let i = 0; i < squareArray[0].length; i++) {
+      coded[i] = [];
     }
 
+    for (let i = 0; i < squareArray.length; i++) {
+      for (let j = 0; j < squareArray[i].length; j++) {
+        coded[j].push(squareArray[i][j])
+      } 
+    }
+    
+    let output = ""
+    for (let i = 0; i < coded.length; i++) {
+      output += coded[i].join("");
+      output += " "
+      
+    }
+    console.log(output)
   }
 }
